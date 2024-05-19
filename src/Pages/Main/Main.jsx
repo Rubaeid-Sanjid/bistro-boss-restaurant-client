9b165dd689ac3b26f341d51ec80a128501d6cfea
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-20 01:05:58 +0600

    added banner section

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../Component/Footer/Footer";
+import Navbar from "../../Component/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
