1196cf4064afd351c3d15bec33d8ba516c50ea4e
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-22 15:59:03 +0600

    useMenu custom hook created

--- src/Component/PopularItems/PopularItems.jsx
+++ src/Component/PopularItems/PopularItems.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
-import { useState } from "react";
 import MenuItem from "../MenuItem/MenuItem";
+import useMenu from "../../Hook/useMenu";
 
 const PopularItems = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(()=>{
-    fetch('menu.json')
-    .then(res=> res.json())
-    .then(data=> {
-      const popularItems = data.filter(item => item.category === 'popular');
-      setMenu(popularItems)
-    })
-  },[])
+  // const [menu, setMenu] = useState([]);
+  // useEffect(()=>{
+  //   fetch('menu.json')
+  //   .then(res=> res.json())
+  //   .then(data=> {
+  //     const popularItems = data.filter(item => item.category === 'popular');
+  //     setMenu(popularItems)
+  //   })
+  // },[])
+  const [menu] = useMenu();
+  const popularItems = menu.filter(item => item.category === 'popular')
   return (
     <div className="container mx-auto px-3 lg:px-12 mb-8 lg:mb-12">
       <SectionTitle
@@ -21,7 +22,7 @@ const PopularItems = () => {
       ></SectionTitle>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {
-          menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
+          popularItems.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
         }
       </div>
     </div>
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,12 +1,22 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Component/Cover/Cover";
+import menuImg from "../../assets/menu/banner3.jpg"
+import PopularItems from "../../Component/PopularItems/PopularItems";
+// import SectionTitle from "../../Component/SectionTitle/SectionTitle";
 
 const Menu = () => {
   return (
-    <div>
+    <div className="">
       <Helmet>
         <title>BISTRO BOSS | Menu</title>
       </Helmet>
-      <h2>Menu</h2>
+      <Cover img={menuImg} title={'OUR MENU'} subtitle={'Would you like to try a dish?'}></Cover>
+      <PopularItems></PopularItems>
+      <Cover img={menuImg} title={'OUR MENU'} subtitle={'Would you like to try a dish?'}></Cover>
+      <PopularItems></PopularItems>
+      <Cover img={menuImg} title={'OUR MENU'} subtitle={'Would you like to try a dish?'}></Cover>
+      <PopularItems></PopularItems>
+      {/* <SectionTitle subtitle={"Don't miss"} title={"TODAY'S OFFER"}></SectionTitle> */}
     </div>
   );
 };
