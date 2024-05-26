45f24591cd5f30703b6def68eeeebd4d95972fca
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-26 13:51:43 +0600

    send user info to database

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet-async";
 import { useContext } from "react";
 import { AuthContext } from "../../Component/AuthProvider/AuthProvider";
 import Swal from "sweetalert2";
+import useAxiosPublic from "../../Hook/useAxiosPublic";
 
 const SignUp = () => {
   const {
@@ -13,29 +14,41 @@ const SignUp = () => {
     formState: { errors },
   } = useForm();
 
-  const {createUser, updateUser} = useContext(AuthContext);
+  const { createUser, updateUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
+  const axiosPublic = useAxiosPublic();
+
   const onSubmit = (data) => {
-    createUser(data.mail, data.password)
-    .then(() => {
+    createUser(data.mail, data.password).then(() => {
       updateUser(data.name, data.photo)
-      .then(() => {
-        reset();
-        Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: "Your account has been created",
-          showConfirmButton: false,
-          timer: 1500
+        .then(() => {
+          const userInfo = {
+            user_name: data.name,
+            user_email: data.mail,
+            user_pass: data.password,
+            user_photo: data.photo
+          };
+          axiosPublic.post("/users", userInfo).then((res) => {
+            if (res.data.insertedId) {
+              reset();
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your account has been created",
+                showConfirmButton: false,
+                timer: 2000,
+              });
+              navigate("/");
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error.message);
         });
-        navigate('/')
-      }).catch((error) => {
-        console.log(error.message);
-      });
-    })
-  }
+    });
+  };
 
   return (
     <>
@@ -43,122 +56,123 @@ const SignUp = () => {
         <title>BISTRO BOSS | Sign Up</title>
       </Helmet>
       <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign Up</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="card-body"
-          >
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="name"
-                // name="name"
-                className="input input-bordered"
-                {...register("name", { required: true })}
-              />
-              {errors.name && (
-                <p role="alert" className="text-red-600 mt-1">
-                  First name is required
-                </p>
-              )}
-            </div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Photo URL</span>
-              </label>
-              <input
-                type="text"
-                placeholder="photoURL"
-                className="input input-bordered"
-                {...register("photo", { required: true })}
-              />
-              {errors.photo && (
-                <p role="alert" className="text-red-600 mt-1">
-                  PhotoURL is required
-                </p>
-              )}
-            </div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                // name="email"
-                className="input input-bordered"
-                {...register("mail", { required: "Email Address is required" })}
-                aria-invalid={errors.mail ? "true" : "false"}
-              />
-              {errors.mail && (
-                <p role="alert" className="text-red-600 mt-1">
-                  {errors.mail.message}
-                </p>
-              )}
-            </div>
-
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                // name="password"
-                className="input input-bordered"
-                {...register("password", {
-                  required: true,
-                  minLength: 6,
-                  maxLength: 20,
-                  pattern: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
-                })}
-              />
-              {errors.password?.type === 'required' && (
-                <p role="alert" className="text-red-600 mt-1">
-                  Password is required
-                </p>
-              )}
-              {errors.password?.type === 'minLength' && (
-                <p role="alert" className="text-red-600 mt-1">
-                  Password must have 6 characters
-                </p>
-              )}
-              {errors.password?.type === 'maxLength' && (
-                <p role="alert" className="text-red-600 mt-1">
-                  Password must have less than 20 characters
-                </p>
-              )}
-              {errors.password?.type === 'pattern' && (
-                <p role="alert" className="text-red-600 mt-1">
-                  Password must have a lower letter, a capital letter, a digit and a special character
-                </p>
-              )}
-            </div>
-
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
-            </div>
-            <p className="text-center">
-              Already have an account ? <Link to={"/login"}>Login</Link>
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign Up</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
             </p>
-          </form>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  // name="name"
+                  className="input input-bordered"
+                  {...register("name", { required: true })}
+                />
+                {errors.name && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    First name is required
+                  </p>
+                )}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="photoURL"
+                  className="input input-bordered"
+                  {...register("photo", { required: true })}
+                />
+                {errors.photo && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    PhotoURL is required
+                  </p>
+                )}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  // name="email"
+                  className="input input-bordered"
+                  {...register("mail", {
+                    required: "Email Address is required",
+                  })}
+                  aria-invalid={errors.mail ? "true" : "false"}
+                />
+                {errors.mail && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    {errors.mail.message}
+                  </p>
+                )}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  // name="password"
+                  className="input input-bordered"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern:
+                      /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/,
+                  })}
+                />
+                {errors.password?.type === "required" && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    Password is required
+                  </p>
+                )}
+                {errors.password?.type === "minLength" && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    Password must have 6 characters
+                  </p>
+                )}
+                {errors.password?.type === "maxLength" && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    Password must have less than 20 characters
+                  </p>
+                )}
+                {errors.password?.type === "pattern" && (
+                  <p role="alert" className="text-red-600 mt-1">
+                    Password must have a lower letter, a capital letter, a digit
+                    and a special character
+                  </p>
+                )}
+              </div>
+
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Sign Up</button>
+              </div>
+              <p className="text-center">
+                Already have an account ? <Link to={"/login"}>Login</Link>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
